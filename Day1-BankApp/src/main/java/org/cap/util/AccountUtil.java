c9af3a3b971b507997198ba48a8b86dd01adb027
radha <pkgopi1348@gmail.com>
2020-04-01 13:39:50 +0530

    scond trail

--- Day1-BankApp/src/main/java/org/cap/util/AccountUtil.java
+++ Day1-BankApp/src/main/java/org/cap/util/AccountUtil.java
@@ -1,10 +1,14 @@
 package org.cap.util;
 
 public class AccountUtil {
-
+  
+	
 	private static int accNo=0;
 	
 	public static int generateAccountNumber(){
 		return accNo++;
+		}
+	private AccountUtil() {
+		
 	}
 }
